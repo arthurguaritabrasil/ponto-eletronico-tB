e487befa3af1764628d303e0d89ffbd2de31ec0e
arthurguaritabrasil <arthurguaritabrasil07@gmail.com>
2024-09-23 21:30:44 -0300

    div alert when point is registered

--- script/components/events.js
+++ script/components/events.js
@@ -5,34 +5,36 @@ import { getLocalization } from "../modules/localization.js";
 
 export function runEventsListeners(confirmarDialog, fecharDialogBtn, confirmarPonto) {
     const btnBaterPonto = document.getElementById("bater-ponto");
-    const mensagemSucesso = document.getElementById("mensagem-sucesso");
+    const fecharAlertaBtn = document.getElementById("fechar-alerta");
+    const alertaRegistro = document.getElementById("alerta-registro-de-ponto");
 
     btnBaterPonto.addEventListener("click", () => {
         confirmarDialog.showModal();
-        mensagemSucesso.style.display = "none";
     });
     fecharDialogBtn.addEventListener("click", () => {
         confirmarDialog.close();
     });
-    confirmarPonto.addEventListener("click", () =>  {
-        criarPonto(confirmarDialog,mensagemSucesso)}
-    );
+    confirmarPonto.addEventListener("click", () => {
+        criarPonto(confirmarDialog, alertaRegistro);
+    });
+    fecharAlertaBtn.addEventListener("click", () => {
+        alertaRegistro.classList.add("hidden");
+    });
 }
 
-async function criarPonto(confirmarDialog, mensagemSucesso) {
+async function criarPonto(confirmarDialog, alertaRegistro) {
     let typeRegister = document.getElementById("opcoes-ponto").value;
-
-    let localization = await getLocalization(); // esperar a promise ser resolvida
+    let localization = await getLocalization(); // Espera pela localização
 
     let ponto = {
-        "nome":"Iuri Games",
+        "nome": "Iuri Games",
         "data": getCurrentDate(),
         "hora": getCurrentHour(),
         "tipo": typeRegister,
         "id": GenerateUUID(),
         "localizacao": {
-            "latitude":localization.latitude,
-            "longitude":localization.longitude,
+            "latitude": localization.latitude,
+            "longitude": localization.longitude,
         }
     };
 
@@ -43,21 +45,12 @@ async function criarPonto(confirmarDialog, mensagemSucesso) {
     localStorage.setItem("lastTypeRegister", typeRegister);
     localStorage.setItem("lastTimeRegister", ponto.hora);
     localStorage.setItem("lastDataRegister", ponto.data);
-    
-    // TO-DO usar div e visibility para mostrar a mensagem.
 
-    /* mensagemSucesso.style.display = "block";
-    timeoutID = setTimeout(() => {
-        let contador = 5;
-        intervalID = setInterval(() => {
-            mensagemSucesso.textContent = `Fechando em ${contador}`;
-            contador--;
-            if (contador === -1) {
-                confirmarDialog.close();
-                mensagemSucesso.style.display = "none";
-                clearInterval(intervalID);
-            }
-        }, 1000);
-    }, 3000);
-    mensagemSucesso.textContent = "O ponto foi criado!"; */
+    confirmarDialog.close();
+
+    alertaRegistro.classList.remove("hidden");
+
+    setTimeout(() => {
+        alertaRegistro.classList.add("hidden");
+    },5000);
 }
